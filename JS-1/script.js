8ca23686ead18a2c2ab70df4f8b891385b7eff73
VikaMelnyk58 <153006020+VikaMelnyk58@users.noreply.github.com>
2024-10-01 23:21:20 +0300

    First

---
+++ JS-1/script.js
@@ -0,0 +1,72 @@
+// a = b * 2;
+
+// const one = 'JavaScript'.length;
+// console.log(one);
+// const two = 'Hello'.toUpperCase();
+// console.log(two);
+
+'use strict'
+
+// var не використовуємо
+// let age = 20;
+// const x = 10000;
+// без ключового слова x =10; дає помилку в суворому режимі
+
+// let username;
+// console.log(username);
+
+// let age = 15;
+// age = 20;
+// console.log(age);
+
+// const age = 15;
+// console.log(age);
+
+// const price = 10;
+// const quantity = 3;
+// let total = price * quantity;
+// console.log(total);
+
+// const x = 1;
+// const X = 1;
+
+// const COLOR = '#fff'
+
+
+// ТИПИ ДАНИХ
+// const age = 20; //number
+// const name = 'Vika'; //string
+// const newAge = '20'; //string
+
+// const isModalOpen = true; //boolean
+// const isLogged = false;
+
+// // null 
+
+// let username;
+// console.log(username); //undefined
+
+// let username;
+// console.log(typeof username); //undefined
+
+// const name = 'Bob';
+// console.log(typeof name); //string
+
+// const age = 15;
+// console.log(typeof age); //number
+
+// const isModalOpen = false;
+// console.log(typeof isModalOpen); //boolean
+
+//console
+const name = 'Vika';
+console.log('Username is ', name); // Username is Vika
+
+// const username = 'Hello, Vika';
+// alert (username);
+
+// const isComing = confirm('Please confirm hotel reservation');
+// console.log(isComing);
+
+const username = prompt('Введіть своє імя')
+console.log(username);
