446a129a16258eb0a79667203dbc8a91a9880b20
VikaMelnyk58 <153006020+VikaMelnyk58@users.noreply.github.com>
2024-10-22 16:01:28 +0300

    Homework №3

---
+++ Homework/JS-3homework/script.js
@@ -0,0 +1,21 @@
+// Завдання 1
+const resolt = 5+5+'5';
+console.log(typeof resolt);
+
+// Завдання 2
+const email = 'vika.melnyk2008@gmail.com';
+console.log(email.length);
+console.log(email.includes('@'));
+
+// Завданя 3
+const one = 'My';
+const two = 'name';
+const three = 'is';
+
+const fullName = `${one} ${two} ${three} Viktoriia`;
+console.log(fullName);
+
+// Завданя 4
+const userName = 'Вікторіє';
+const payment = 25000;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень.`);
