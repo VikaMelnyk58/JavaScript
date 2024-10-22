c3e8ab57ae6c38ac40414fefa2c43352bb17e64a
VikaMelnyk58 <153006020+VikaMelnyk58@users.noreply.github.com>
2024-10-22 19:58:13 +0300

    Lessons №4

---
+++ Lessons/JS-4/script.js
@@ -0,0 +1,68 @@
+// false, 0, NaN, null, undefined,"", '' = false
+// 1, 'hello', '0' = true
+
+// const number = 20;
+// const result = number > 10 && number < 30
+// console.log(result);
+
+// const num = 5;
+// const newRe = num < 10 || num > 30;
+// console.log(newRe);
+
+// console.log(!1);
+// console.log(!false);
+// console.log(!5);
+
+
+// // if
+// let cost = 0;
+// const sub = 'pro';
+// if (sub === 'pro') {
+//     cost = 100;
+// }
+// console.log(cost);
+
+
+// // if...else
+// let cost;
+// const sub = 'premium';
+// if(sub === 'pro'){
+//     cost = 100;
+// } else{
+//     cost = 200;
+// }
+// console.log(cost);
+
+
+// // else...if
+// let cost;
+// const sub = 'premium';
+
+// if ( sub === 'free') {
+//     cost = 0;
+// } else if (sub === 'pro') {
+//     cost = 100;
+// } else if ( sub === 'premium') {
+//     cost = 200;
+// } else {
+//     console.log('invalid')
+// }
+
+
+// // {} ? {} : {}
+// let type;
+// const age = 27;
+
+// if (age >= 18){
+//     type = 'adult';
+// } else {
+//     type = 'child';
+// }
+
+
+// const newAge = 27;
+// const newType = newAge >= 18 ? 'adult' : 'child';
+
+// const num1 = 5;
+// const num2 = 10;
+// const bigger = num1 > num2 ? num1 : num2;
